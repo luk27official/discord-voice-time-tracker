c9f944254c8e9de2a298b37d1824501eb46da2b4
Lukas Polak <admin@lukaspolak.cz>
2024-04-15 00:14:10 +0200

    Init, solution

---
+++ README.md
@@ -0,0 +1,15 @@
+# Discord voice time-tracker
+
+A Discord bot that tracks total time spent in the voice channels.
+
+## Installation
+
+1. Create a Discord bot with Administrator permissions
+2. Create `.env` file with `DISCORD_TOKEN=<your_token>`
+3. Invite the bot to your server with Administrator permissions
+4. `pip install discord.py python-dotenv python-dateutil`
+5. `python bot.py`
+
+## Usage
+
+Type `/ustat` for personal stats or `/gstat` for top10 users in your Discord channel.
---
+++ bot.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+
+import discord
+from discord.ext import tasks
+from discord.ext import commands
+from dateutil.relativedelta import relativedelta as rd
+
+from dotenv import load_dotenv
+from datetime import datetime
+import os
+import json
+
+# Initialize the Discord client
+bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+
+    await bot.tree.sync()
+    my_background_task.start()
+
+
+@tasks.loop(seconds=60)
+async def my_background_task():
+    await bot.wait_until_ready()
+
+    active_members = []
+    disconnected_members = []
+
+    print(f"Checking for active members... It is {datetime.now()}")
+
+    # open a file with the current users
+    with open("current_users.json", "r+") as f:
+        # first, read the users from the JSON
+        # looks like this: [{"id": "123", "startTime": "2021-09-01T12:00:00"}]
+        file_content = f.read()
+        if not file_content:
+            current_members = []
+        current_members = json.loads(file_content)
+
+        # get the active members
+        for guild in bot.guilds:
+            for channel in guild.voice_channels:
+                for member in channel.members:
+                    active_members.append(member)
+
+        # check if the user is already in the list
+        for member in active_members:
+            # if the user is not in the list, add it
+            if member.id not in [user["id"] for user in current_members]:
+                current_members.append({"id": member.id, "startTime": str(datetime.now())})
+
+        # remove the users that are not in the active_members list (they left the voice channel)
+        for user in current_members:
+            if user["id"] not in [member.id for member in active_members]:
+                current_members.remove(user)
+                disconnected_members.append(user)
+
+        # write the users back to the file
+        f.seek(0)
+        f.truncate()
+        json.dump(current_members, f)
+
+    # write the disconnected members to a file, calculate the time spent in the voice channel
+    # the final format is [{ "id": "123", "totalTime": 100 }]
+    with open("disconnected_users.json", "r+") as f:
+        file_content = f.read()
+        if not file_content:
+            final_users = []
+        final_users = json.loads(file_content)
+
+        for member in disconnected_members:
+            startTime = datetime.fromisoformat(member["startTime"])
+            timeSpent = datetime.now() - startTime
+            # find the user in the list and update the time
+            found = False
+
+            for user in final_users:
+                if user["id"] == member["id"]:
+                    user["totalTime"] += timeSpent.total_seconds()
+                    found = True
+                    break
+
+            if not found:
+                final_users.append({"id": member["id"], "totalTime": timeSpent.total_seconds()})
+
+        # write the users back to the file
+        f.seek(0)
+        f.truncate()
+        json.dump(final_users, f)
+
+
+@bot.tree.command(name="ustat", description="Get your user statistics")
+async def user_statistics(interaction):
+    # id to search is either the author or the id given in the command
+    # TODO: get the id from the command
+    id_to_search = interaction.user.id
+
+    # get the stats for a user given in the command
+    with open("disconnected_users.json", "r") as f:
+        users = json.load(f)
+        for user in users:
+            if user["id"] == id_to_search:
+                await interaction.response.send_message(
+                    f"User <@!{id_to_search}> has spent {user['totalTime']} seconds in voice channels."
+                )
+                return
+
+    await interaction.response.send_message(f"User <@!{id_to_search}> has not spent any time in voice channels.")
+
+
+@bot.tree.command(name="gstat", description="Get the top10 for the whole server")
+async def guild_statistics(interaction):
+    with open("disconnected_users.json", "r") as f:
+        users = json.load(f)
+
+        # sort the users by the total time spent in voice channels
+        users = sorted(users, key=lambda x: x["totalTime"], reverse=True)
+
+        # get the top 10 users
+        top10 = users[:10]
+
+        # get the top 10 users
+        top10_users = []
+        fmt = '{0.days} days {0.hours} hours {0.minutes} minutes'
+        for user in top10:
+            top10_users.append(f"<@!{user['id']}>: {fmt.format(rd(seconds=int(user["totalTime"])))}")
+
+        final_msg = "Top 10 active users:\n" + "\n".join([f"{i+1}. {user}" for i, user in enumerate(top10_users)])
+
+        await interaction.response.send_message(final_msg)
+
+
+def main():
+    # python3 -m pip install discord.py python-dotenv python-dateutil
+    load_dotenv()
+    TOKEN = os.getenv("DISCORD_TOKEN")
+    bot.run(TOKEN)
+
+
+if __name__ == "__main__":
+    main()
